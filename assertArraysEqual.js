36476c035ddbf11e6a961d1de0bdcbc38295cdb1
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-20 00:54:28 +0000

    Adding another option to print the correct output, by importing the util library's inspect function. I kept it commented because the concatenation version is still better

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,11 +13,22 @@ const eqArrays = (firstArray, secondArray) => {
 };
 
 // FUNCTION IMPLEMENTATION
-const assertArraysEqual = function (actual, expected) {
+/*
+* the commented lines are alternatives if we don't want to use concatenation. We have two options:
+* 1. use the util library's inspect function (first we import (const inspect = require('util').inspect)
+* then use inspect(actual))
+* 2. use literal template. When using literal template, we got some odd results when comparing
+* [undefined] and [''] arrays, the output was showing the right message (failed), but it was
+* also showing [] !== []. To overcome this problem, JSON.stringify and ternary operators were used.
+*/
+const assertArraysEqual = (actual, expected) => {
+  // const inspect = require('util').inspect;
   if (eqArrays(actual, expected)) {
+    // console.log(`\u2705\u2705\u2705 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
     // console.log(`\u2705\u2705\u2705 Assertion Passed: [${actual}] === [${expected}]`);
     console.log("\u2705\u2705\u2705 Assertion Passed:", actual, "===", expected);
   } else {
+    // console.log(`\u274C\u274C\u274C Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
     // console.log(`\u274C\u274C\u274C Assertion Failed: ${actual[0] === undefined ? `[${JSON.stringify(undefined)}]` : JSON.stringify(actual)} !== ${expected[0] === undefined ? JSON.stringify(undefined) : JSON.stringify(expected)}`);
     console.log("\u274C\u274C\u274C Assertion Failed:", actual, "!==", expected);
   }
