88c80234fb8ef31b314c9d7f4ed850ec5bfe530e
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-24 17:31:18 +0000

    Modularizing assertEqual.js and head.js and include the test folder with files to test both of the mentioned functions

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("dog", "dog");
-assertEqual("cat", "Cat");
-assertEqual(0.1, 1 / 10);
-assertEqual(10, 15);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C\u274C\u274C Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // FUNCTION IMPLEMENTATION
 const head = array => array[0];
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+module.exports = head;
