843671cd4f4475b268224eb9ed4259061bb3bb39
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-24 19:02:56 +0000

    Refactored middleTest.js and tailTest.js so that the tests run with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 const tail = require('../tail');
+describe('#tail', () => {
+  it('returns [6, 7] for [5, 6, 7]', () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+  it('returns ["6", "7"] for ["5", "6", "7"]', () => {
+    assert.deepEqual(tail(["5", "6", "7"]), ["6", "7"]);
+  });
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('returns [] for [1]', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
 
-// TEST CODE
-assertArraysEqual(tail([5, 6, 7]), [6, 7]);
-assertArraysEqual(tail([5, 6, 7]), [7, 6]);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertArraysEqual(tail([1]), []);
-assertArraysEqual(tail([]), []);
+  });
+});
