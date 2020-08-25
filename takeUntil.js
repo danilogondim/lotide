ee5904a242b41a9ed01a57ef556f940d996097ea
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-25 11:24:16 +0000

    Refactored takeUntil.js and created its test file

--- takeUntil.js
+++ takeUntil.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 // a function that returns a slice of a given array with elements taken from the beginning until the callback function returns a truthy value.
 
 const takeUntil = (array, callback) => {
@@ -12,21 +11,5 @@ const takeUntil = (array, callback) => {
   }
   return output;
 };
-
-// TEST CODE
-const assertArraysEqual = require('./assertArraysEqual');
-
-// tests
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const data3 = ["chocolate","chocolate", "chocolate", "raisin"];
-const data4 = ["chocolate", "raisin"];
-const data5 = ["raisin"];
-
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
-assertArraysEqual(takeUntil(data3, x => x === 'raisin'), ["chocolate","chocolate", "chocolate"]);
-assertArraysEqual(takeUntil(data4, x => x === 'raisin'), ["chocolate"]);
-assertArraysEqual(takeUntil(data5, x => x === 'raisin'), []);
 
 module.exports = takeUntil;
---
+++ test/takeUntilTest.js
@@ -0,0 +1,20 @@
+
+// TEST CODE
+// const assertArraysEqual = require('../assertArraysEqual');
+const takeUntil = require('../takeUntil');
+const { assert } = require('chai');
+
+describe('#takeUntil', () => {
+  it('returns [1, 5, 7] for ([1, 5, 7, -1, 2, 4, 5], x => x < 0)', () => {
+    const actual = takeUntil([1, 5, 7, -1, 2, 4, 5], x => x < 0);
+    assert.deepEqual(actual, [1, 5, 7]);
+  });
+  it('returns ["chocolate", "chocolate"] for (["chocolate", "chocolate", "raisin"], x => x === "raisin")', () => {
+    const actual = takeUntil(["chocolate", "chocolate", "raisin"], x => x === "raisin");
+    assert.deepEqual(actual, ["chocolate", "chocolate"]);
+  });
+  it('returns [] for (["raisin"], x => x === "raisin")', () => {
+    const actual = takeUntil(["raisin"], x => x === "raisin");
+    assert.deepEqual(actual, []);
+  });
+});
